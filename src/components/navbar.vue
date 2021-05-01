8a6bf238148a2e139dab6fee380c9fedbfa53911
bedo59 <elhadybedo2@gmail.com>
2021-05-01 06:13:06 +0200

    inip

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,21 +1,29 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="/"
+      <router-link to="/"
         ><img alt="Vue logo" src="../assets/log.png"
-      /></b-navbar-brand>
+      /></router-link>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item href="/gam3a">الجامعات</b-nav-item>.
-          <b-nav-item href="/ma3had">المعاهد</b-nav-item>.
-          <b-nav-item href="/tass2el">التسجيل</b-nav-item>.
-          <b-nav-item href="about">عننا</b-nav-item>
+          <div class="aqq">
+            <router-link to="/about">عننا</router-link>|
+            <router-link to="/tass2el">التسجيل</router-link> |
+            <router-link to="/ma3had">المعاهد</router-link> |
+            <router-link to="/gam3a">الجامعات</router-link> 
+          </div>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
   </div>
 </template>
+
+<style scoped>
+.aqq {
+  background-color: cornsilk;
+}
+</style>
